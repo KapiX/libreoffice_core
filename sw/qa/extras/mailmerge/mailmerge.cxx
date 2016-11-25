LibreOffice/core

commit e637b6743a506ef74c93ccbe15ab6642f3baa34f

MM: move email subject and address into descriptor
Both are not related to the DBManager, but the mail merge job.

While at it, also drop the insane bSubjectIsFilename handling, add
an independent sPath, rename sAddressFromColumn to a more general
sDBcolumn, as it's also used for file name lookup in FILE mode, and
unify the DB column data handling.

Change-Id: I1b583f4a67279a229329ba7cd15fb8d2c215737c

sw/inc/dbmgr.hxx
@@ -149,8 +149,9 @@ struct SwMergeDescriptor
     OUString                                            sSaveToFilterOptions;
     css::uno::Sequence< css::beans::PropertyValue >     aSaveToFilterData;
 
+    OUString                                            sPath;
+
     OUString                                            sSubject;
-    OUString                                            sAddressFromColumn;
     OUString                                            sMailBody;
     OUString                                            sAttachmentName;
     css::uno::Sequence< OUString >                      aCopiesTo;
@@ -163,7 +164,8 @@ struct SwMergeDescriptor
 
     bool                                            bPrintAsync;
     bool                                            bCreateSingleFile;
-    bool                                            bSubjectIsFilename;
+
+    OUString                                            sDBcolumn;
 
     SwMailMergeConfigItem*                              pMailMergeConfigItem;
 
@@ -179,7 +181,6 @@ struct SwMergeDescriptor
         bSendAsAttachment( false ),
         bPrintAsync( false ),
         bCreateSingleFile( false ),
-        bSubjectIsFilename( false ),
         pMailMergeConfigItem(nullptr)
         {}
 
@@ -194,8 +195,6 @@ class SW_DLLPUBLIC SwDBManager
 {
 friend class SwConnectionDisposedListener_Impl;
 
-    OUString            sEMailAddrField;      ///< Mailing: Column name of email address.
-    OUString            sSubject;           ///< Mailing: Subject
     bool            m_bCancel;            ///< Mail merge canceled.
     bool            bInitDBFields : 1;
     bool            bInMerge    : 1;    ///< merge process active
@@ -258,10 +257,6 @@ public:
     /// Initialize data fields that lack name of database.
     inline bool     IsInitDBFields() const  { return bInitDBFields; }
     inline void     SetInitDBFields(bool b) { bInitDBFields = b;    }
-
-    /// Mailing: Set email data.
-    inline void     SetEMailColumn(const OUString& sColName) { sEMailAddrField = sColName; }
-    inline void     SetSubject(const OUString& sSbj) { sSubject = sSbj; }
 
     /// Fill listbox with all table names of a database.
     bool            GetTableNames(ListBox* pListBox, const OUString& rDBName );

sw/source/uibase/dbui/dbmgr.cxx
@@ -987,16 +987,20 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
 
     uno::Reference< beans::XPropertySet > xColumnProp;
     {
-        bool bColumnName = !sEMailAddrField.isEmpty();
-
-        if (bColumnName)
+        // Check for (mandatory) email or (optional) filename column
+        SwDBFormatData aColumnDBFormat;
+        bool bColumnName = !rMergeDescriptor.sDBcolumn.isEmpty();
+        if( bColumnName )
         {
             uno::Reference< sdbcx::XColumnsSupplier > xColsSupp( pImpl->pMergeData->xResultSet, uno::UNO_QUERY );
             uno::Reference<container::XNameAccess> xCols = xColsSupp->getColumns();
-            if(!xCols->hasByName(sEMailAddrField))
+            if( !xCols->hasByName( rMergeDescriptor.sDBcolumn ) )
                 return false;
-            uno::Any aCol = xCols->getByName(sEMailAddrField);
+            uno::Any aCol = xCols->getByName( rMergeDescriptor.sDBcolumn );
             aCol >>= xColumnProp;
+
+            aColumnDBFormat.xFormatter = pImpl->pMergeData->xFormatter;
+            aColumnDBFormat.aNullDate  = pImpl->pMergeData->aNullDate;
         }
 
         // Try saving the source document
@@ -1169,33 +1173,36 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
             SwView*            pWorkView             = nullptr;
             SwDoc*             pWorkDoc              = nullptr;
             SwDBManager*       pWorkDocOrigDBManager = nullptr;
+            bool               bWorkDocInitialized   = false;
 
             do
             {
                 nStartRow = pImpl->pMergeData ? pImpl->pMergeData->xResultSet->getRow() : 0;
                 {
-                    OUString sPath(sSubject);
+                    OUString sPath = rMergeDescriptor.sPath;
+                    OUString sColumnData;
 
-                    OUString sAddress;
-                    if( !bMT_EMAIL && bColumnName )
+                    // Read the indicated data column, which should contain a valid mail
+                    // address or an optional file name
+                    if( bMT_EMAIL || bColumnName )
                     {
-                        SwDBFormatData aDBFormat;
-                        aDBFormat.xFormatter = pImpl->pMergeData->xFormatter;
-                        aDBFormat.aNullDate = pImpl->pMergeData->aNullDate;
-                        sAddress = GetDBField( xColumnProp, aDBFormat);
-                        if (sAddress.isEmpty())
-                            sAddress = "_";
-                        sPath += sAddress;
+                        sColumnData = GetDBField( xColumnProp, aColumnDBFormat );
+                        if( !bMT_EMAIL )
+                        {
+                            if (sColumnData.isEmpty())
+                                sColumnData = "_";
+                            sPath += sColumnData;
+                        }
                     }
 
                     // create a new temporary file name - only done once in case of bCreateSingleFile
-                    if( bNeedsTempFiles && ( 1 == nDocNo || !bCreateSingleFile ))
+                    if( bNeedsTempFiles && ( !bWorkDocInitialized || !bCreateSingleFile ))
                     {
                         INetURLObject aEntry(sPath);
                         OUString sLeading;
                         //#i97667# if the name is from a database field then it will be used _as is_
-                        if( !sAddress.isEmpty() )
-                            sLeading = sAddress;
+                        if( !sColumnData.isEmpty() )
+                            sLeading = sColumnData;
                         else
                             sLeading = aEntry.GetBase();
                         aEntry.removeSegment();
@@ -1203,8 +1210,6 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                         OUString sExt(comphelper::string::stripStart(pStoreToFilter->GetDefaultExtension(), '*'));
                         aTempFile.reset(
                             new utl::TempFile(sLeading, true, &sExt, &sPath));
-                        if( rMergeDescriptor.bSubjectIsFilename )
-                            aTempFile->EnableKillingFile();
                         if( !aTempFile->IsValid() )
                         {
                             ErrorHandler::HandleError( ERRCODE_IO_NOTSUPPORTED );
@@ -1237,7 +1242,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                         // Create a copy of the source document and work with that one instead of the source.
                         // If we're not in the single file mode (which requires modifying the document for the merging),
                         // it is enough to do this just once. Currently PDF also has to be treated special.
-                        if( 1 == nDocNo || bCreateSingleFile || bIsPDFexport )
+                        if( !bWorkDocInitialized || bCreateSingleFile || bIsPDFexport )
                         {
                             assert( !xWorkDocSh.Is());
                             // copy the source document
@@ -1330,7 +1335,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                             if( targetDocPageCount % 2 == 1 )
                                 ++targetDocPageCount; // Docs always start on odd pages (so offset must be even).
                             SwNodeIndex appendedDocStart = pTargetDoc->AppendDoc(*rWorkShell.GetDoc(),
-                                nStartingPageNo, pTargetPageDesc, nDocNo == 1, targetDocPageCount);
+                                nStartingPageNo, pTargetPageDesc, !bWorkDocInitialized, targetDocPageCount);
                             targetDocPageCount += rWorkShell.GetPageCnt();
                             if ( (nMaxDumpDocs < 0) || (nDocNo <= nMaxDumpDocs) )
                                 lcl_SaveDebugDoc( xTargetDocShell, "MergeDoc" );
@@ -1346,7 +1351,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                         }
                         else if( bMT_PRINTER )
                         {
-                            if( 1 == nDocNo ) // set up printing only once at the beginning
+                            if( !bWorkDocInitialized ) // set up printing only once at the beginning
                             {
                                 uno::Sequence< beans::PropertyValue > aOptions( rMergeDescriptor.aPrintOptions );
                                 lcl_PreparePrinterOptions( rMergeDescriptor.aPrintOptions, false, aOptions );
@@ -1374,11 +1379,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                             }
                             if( bMT_EMAIL )
                             {
-                                SwDBFormatData aDBFormat;
-                                aDBFormat.xFormatter = pImpl->pMergeData->xFormatter;
-                                aDBFormat.aNullDate = pImpl->pMergeData->aNullDate;
-                                OUString sMailAddress = GetDBField( xColumnProp, aDBFormat);
-                                if(!SwMailMergeHelper::CheckMailAddress( sMailAddress ))
+                                if( !SwMailMergeHelper::CheckMailAddress( sColumnData ) )
                                 {
                                     OSL_FAIL("invalid e-Mail address in database column");
                                 }
@@ -1388,9 +1389,10 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                                     uno::Reference< mail::XMailMessage > xMessage = pMessage;
                                     if(rMergeDescriptor.pMailMergeConfigItem->IsMailReplyTo())
                                         pMessage->setReplyToAddress(rMergeDescriptor.pMailMergeConfigItem->GetMailReplyTo());
-                                    pMessage->addRecipient( sMailAddress );
+                                    pMessage->addRecipient( sColumnData );
                                     pMessage->SetSenderAddress( rMergeDescriptor.pMailMergeConfigItem->GetMailAddress() );
                                     OUString sBody;
+
                                     if(rMergeDescriptor.bSendAsAttachment)
                                     {
                                         sBody = rMergeDescriptor.sMailBody;
@@ -1457,6 +1459,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                         }
                     }
                 }
+                bWorkDocInitialized = true;
                 nDocNo++;
                 nEndRow = pImpl->pMergeData ? pImpl->pMergeData->xResultSet->getRow() : 0;
 
@@ -1510,7 +1513,15 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
                 {
                     // save merged document
                     assert( aTempFile.get() );
-                    INetURLObject aTempFileURL( rMergeDescriptor.bSubjectIsFilename ? sSubject : aTempFile->GetURL());
+                    INetURLObject aTempFileURL;
+                    if( rMergeDescriptor.sPath.isEmpty() )
+                        aTempFileURL.SetURL( aTempFile->GetURL() );
+                    else
+                    {
+                        aTempFileURL.SetURL( rMergeDescriptor.sPath );
+                        // remove the unneeded temporary file
+                        aTempFile->EnableKillingFile();
+                    }
                     bNoError = lcl_SaveDoc( &aTempFileURL, pStoreToFilter,
                             pStoreToFilterOptions, &rMergeDescriptor.aSaveToFilterData,
                             bIsPDFexport, xTargetDocShell, *pTargetShell );
@@ -2860,12 +2871,11 @@ void SwDBManager::ExecuteFormLetter( SwWrtShell& rSh,
 
                     SwMergeDescriptor aMergeDesc( pImpl->pMergeDialog->GetMergeType(), pView->GetWrtShell(), aDescriptor );
                     aMergeDesc.sSaveToFilter = pImpl->pMergeDialog->GetSaveFilter();
-                    aMergeDesc.bCreateSingleFile = pImpl->pMergeDialog->IsSaveSingleDoc() && pImpl->pMergeDialog->GetMergeType() != DBMGR_MERGE_PRINTER;
-                    aMergeDesc.bSubjectIsFilename = aMergeDesc.bCreateSingleFile;
+                    aMergeDesc.bCreateSingleFile = pImpl->pMergeDialog->IsSaveSingleDoc();
+                    aMergeDesc.sPath = pImpl->pMergeDialog->GetTargetURL();
                     if( !aMergeDesc.bCreateSingleFile && pImpl->pMergeDialog->IsGenerateFromDataBase() )
                     {
-                        aMergeDesc.sAddressFromColumn = pImpl->pMergeDialog->GetColumnName();
-                        aMergeDesc.sSubject = pImpl->pMergeDialog->GetPath();
+                        aMergeDesc.sDBcolumn = pImpl->pMergeDialog->GetColumnName();
                     }
 
                     MergeNew(aMergeDesc);

sw/source/uibase/uno/unomailmerge.cxx
@@ -700,7 +700,6 @@ uno::Any SAL_CALL SwXMailMerge::execute(
         aMergeDesc.pMailMergeConfigItem = pMMConfigItem.get();
         break;
     case MailMergeType::FILE:
-    case MailMergeType::MAIL:
         {
             INetURLObject aURLObj;
             aURLObj.SetSmartProtocol( INetProtocol::File );
@@ -731,23 +730,22 @@ uno::Any SAL_CALL SwXMailMerge::execute(
             if (!aPath.isEmpty() && !aPath.endsWith(aDelim))
                 aPath += aDelim;
             if (bCurFileNameFromColumn)
-                pMgr->SetEMailColumn( aCurFileNamePrefix );
+                aMergeDesc.sDBcolumn = aCurFileNamePrefix;
             else
             {
                 aPath += aCurFileNamePrefix;
-                pMgr->SetEMailColumn( OUString() );
             }
-            pMgr->SetSubject( aPath );
-            if(MailMergeType::FILE == nCurOutputType)
+
+            aMergeDesc.sPath = aPath;
+            aMergeDesc.sSaveToFilter = m_sSaveFilter;
+            aMergeDesc.sSaveToFilterOptions = m_sSaveFilterOptions;
+            aMergeDesc.aSaveToFilterData = m_aSaveFilterData;
+            aMergeDesc.bCreateSingleFile = m_bSaveAsSingleFile;
+        }
+        break;
+    case MailMergeType::MAIL:
             {
-                aMergeDesc.sSaveToFilter = m_sSaveFilter;
-                aMergeDesc.sSaveToFilterOptions = m_sSaveFilterOptions;
-                aMergeDesc.aSaveToFilterData = m_aSaveFilterData;
-                aMergeDesc.bCreateSingleFile = m_bSaveAsSingleFile;
-            }
-            else
-            {
-                pMgr->SetEMailColumn( m_sAddressFromColumn );
+                aMergeDesc.sDBcolumn = m_sAddressFromColumn;
                 if(m_sAddressFromColumn.isEmpty())
                     throw RuntimeException("Mail address column not set.", static_cast < cppu::OWeakObject * > ( this ) );
                 aMergeDesc.sSaveToFilter     = m_sAttachmentFilter;
@@ -769,7 +767,6 @@ uno::Any SAL_CALL SwXMailMerge::execute(
                 if( !aMergeDesc.xSmtpServer.is() || !aMergeDesc.xSmtpServer->isConnected())
                     throw RuntimeException("Failed to connect to mail server.", static_cast < cppu::OWeakObject * > ( this ) );
             }
-        }
         break;
     }
 

sw/inc/swabstdlg.hxx
@@ -149,14 +149,15 @@ public:
     virtual bool IsSaveSingleDoc() const = 0;
     virtual bool IsGenerateFromDataBase() const = 0;
     virtual OUString GetColumnName() const = 0;
-    virtual OUString GetPath() const = 0;
-
+    virtual OUString GetTargetURL() const = 0;
 };
+
 class AbstractMailMergeCreateFromDlg : public VclAbstractDialog
 {
 public:
     virtual bool    IsThisDocument() const = 0;
 };
+
 class AbstractMailMergeFieldConnectionsDlg : public VclAbstractDialog
 {
 public:

sw/source/ui/dialog/swdlgfact.cxx
@@ -512,9 +512,9 @@ OUString AbstractMailMergeDlg_Impl::GetColumnName() const
     return pDlg->GetColumnName();
 }
 
-OUString AbstractMailMergeDlg_Impl::GetPath() const
+OUString AbstractMailMergeDlg_Impl::GetTargetURL() const
 {
-    return pDlg->GetPath();
+    return pDlg->GetTargetURL();
 }
 
 bool AbstractMailMergeCreateFromDlg_Impl::IsThisDocument() const

sw/source/ui/dialog/swdlgfact.hxx
@@ -284,7 +284,7 @@ class AbstractMailMergeDlg_Impl : public AbstractMailMergeDlg
     virtual bool IsSaveSingleDoc() const override;
     virtual bool IsGenerateFromDataBase() const override;
     virtual OUString GetColumnName() const override;
-    virtual OUString GetPath() const override;
+    virtual OUString GetTargetURL() const override;
 };
 
 class SwMailMergeCreateFromDlg;

KapiX/libreoffice_core

commit d3d9d26372b0da5626389aef835f81129bd1882a

tdf#102010 Never overwrite MM files via UNO
Fixes the regression introduced by
  commit e637b6743a506ef74c93ccbe15ab6642f3baa34f

This commit removed the crazy bSubjectIsFilename handling,
where I didn't understood the case of bSubjectIsFilename
and a user supplied prefix.

Mail merge to files never overwrites an existing document,
but there is the special case, when a user selects a target
filename in the MM dialog for single file MM.

Should be fixed by a successive commmit, reverting this and
removing an existing file before starting the MM job.

Change-Id: Idda487023e6984de9c1e701fc088a6b7f92e9847

sw/CppunitTest_sw_mailmerge.mk
@@ -21,6 +21,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sw_mailmerge, \
     sfx \
     sw \
     test \
+    tl \
     unotest \
     utl \
 ))

sw/inc/dbmgr.hxx
@@ -162,7 +162,23 @@ struct SwMergeDescriptor
      * @defgroup file Mail merge as File settings
      * @addtogroup file
      * @{ */
-    OUString                                            sPath;
+
+    /**
+     * Basename incl. the path for the generated files.
+     *
+     * The final filename will be created by concating a number to prevent
+     * overwriting an existing file and the extension based on the filter
+     * settings.
+     */
+    OUString                                            sPrefix;
+    /**
+     * Use the sPrefix as the target filename also overwriting an existing
+     * target file.
+     *
+     * Just used for the internal mail merge dialogs as mail merge never
+     * overwrites existing files (see SwDBManager::ExecuteFormLetter).
+     */
+    bool                                                bPrefixIsFilename;
     /** @} */
 
     /**
@@ -205,6 +221,7 @@ struct SwMergeDescriptor
         rSh(rShell),
         rDescriptor(rDesc),
         bCreateSingleFile( false ),
+        bPrefixIsFilename( false ),
         bSendAsHTML( true ),
         bSendAsAttachment( false ),
         pMailMergeConfigItem( nullptr )

sw/source/uibase/dbui/dbmgr.cxx
@@ -1098,11 +1098,13 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
     bool bCheckSingleFile_ = rMergeDescriptor.bCreateSingleFile;
     if( bMT_EMAIL )
     {
+        assert( !rMergeDescriptor.bPrefixIsFilename );
         assert( bMT_EMAIL && !bCheckSingleFile_ );
         bCheckSingleFile_ = false;
     }
     else if( bMT_SHELL || bMT_PRINTER )
     {
+        assert( !rMergeDescriptor.bPrefixIsFilename );
         assert( (bMT_SHELL || bMT_PRINTER) && bCheckSingleFile_ );
         bCheckSingleFile_ = true;
     }
@@ -1318,7 +1320,7 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
         // create a new temporary file name - only done once in case of bCreateSingleFile
         if( bNeedsTempFiles && ( !bWorkDocInitialized || !bCreateSingleFile ))
         {
-            OUString sPath = rMergeDescriptor.sPath;
+            OUString sPrefix = rMergeDescriptor.sPrefix;
             OUString sLeading;
 
             //#i97667# if the name is from a database field then it will be used _as is_
@@ -1331,14 +1333,14 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
             }
             else
             {
-                INetURLObject aEntry( sPath );
+                INetURLObject aEntry( sPrefix );
                 sLeading = aEntry.GetBase();
                 aEntry.removeSegment();
-                sPath = aEntry.GetMainURL( INetURLObject::NO_DECODE );
+                sPrefix = aEntry.GetMainURL( INetURLObject::NO_DECODE );
             }
 
             OUString sExt(comphelper::string::stripStart(pStoreToFilter->GetDefaultExtension(), '*'));
-            aTempFile.reset( new utl::TempFile(sLeading, sColumnData.isEmpty(), &sExt, &sPath, true) );
+            aTempFile.reset( new utl::TempFile(sLeading, sColumnData.isEmpty(), &sExt, &sPrefix, true) );
             if( !aTempFile->IsValid() )
             {
                 ErrorHandler::HandleError( ERRCODE_IO_NOTSUPPORTED );
@@ -1559,11 +1561,11 @@ bool SwDBManager::MergeMailFiles(SwWrtShell* pSourceShell,
             // save merged document
             assert( aTempFile.get() );
             INetURLObject aTempFileURL;
-            if( rMergeDescriptor.sPath.isEmpty() )
+            if( rMergeDescriptor.sPrefix.isEmpty() || !rMergeDescriptor.bPrefixIsFilename )
                 aTempFileURL.SetURL( aTempFile->GetURL() );
             else
             {
-                aTempFileURL.SetURL( rMergeDescriptor.sPath );
+                aTempFileURL.SetURL( rMergeDescriptor.sPrefix );
                 // remove the unneeded temporary file
                 aTempFile->EnableKillingFile();
             }
@@ -2863,7 +2865,8 @@ void SwDBManager::ExecuteFormLetter( SwWrtShell& rSh,
         SwMergeDescriptor aMergeDesc( pImpl->pMergeDialog->GetMergeType(), rSh, aDescriptor );
         aMergeDesc.sSaveToFilter = pImpl->pMergeDialog->GetSaveFilter();
         aMergeDesc.bCreateSingleFile = pImpl->pMergeDialog->IsSaveSingleDoc();
-        aMergeDesc.sPath = pImpl->pMergeDialog->GetTargetURL();
+        aMergeDesc.bPrefixIsFilename = aMergeDesc.bCreateSingleFile;
+        aMergeDesc.sPrefix = pImpl->pMergeDialog->GetTargetURL();
         if( !aMergeDesc.bCreateSingleFile && pImpl->pMergeDialog->IsGenerateFromDataBase() )
         {
             aMergeDesc.sDBcolumn = pImpl->pMergeDialog->GetColumnName();

sw/source/uibase/uno/unomailmerge.cxx
@@ -738,7 +738,7 @@ uno::Any SAL_CALL SwXMailMerge::execute(
                 aPath += aCurFileNamePrefix;
             }
 
-            aMergeDesc.sPath = aPath;
+            aMergeDesc.sPrefix = aPath;
             aMergeDesc.sSaveToFilter = m_sSaveFilter;
             aMergeDesc.sSaveToFilterOptions = m_sSaveFilterOptions;
             aMergeDesc.aSaveToFilterData = m_aSaveFilterData;

sw/qa/extras/mailmerge/mailmerge.cxx
@@ -25,6 +25,8 @@
 #include <com/sun/star/sdbc/XRowSet.hpp>
 #include <com/sun/star/sdbcx/XRowLocate.hpp>
 
+#include <tools/urlobj.hxx>
+
 #include <wrtsh.hxx>
 #include <ndtxt.hxx>
 #include <swdtflvr.hxx>
@@ -145,7 +147,8 @@ public:
                                          ( OUString( m_directories.getURLFromSrc(mpTestDocumentPath) + OUString::createFromAscii(filename)) ) ) ) );
         mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_DATA_SOURCE_NAME ), uno::Any( aDBName ) ) );
         mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_OUTPUT_URL ), uno::Any( aWorkDir ) ) );
-        mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_FILE_NAME_PREFIX ), uno::Any( aPrefix )) );
+        if (file)
+            mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_FILE_NAME_PREFIX ), uno::Any( aPrefix )) );
 
         if (bPrefixIsColumn)
             mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_FILE_NAME_FROM_COLUMN ), uno::Any( true ) ) );
@@ -173,7 +176,7 @@ public:
 
     }
 
-    void executeMailMerge()
+    void executeMailMerge( bool bDontLoadResult = false )
     {
         uno::Sequence< beans::NamedValue > aSeqMailMergeArgs = comphelper::containerToSequence( mMMargs );
         uno::Any res = mxJob->execute( aSeqMailMergeArgs );
@@ -196,6 +199,8 @@ public:
                 bOk &= rValue >>= mnCurOutputType;
             else if (rName == UNO_NAME_FILE_NAME_FROM_COLUMN)
                 bOk &= rValue >>= bMMFilenameFromColumn;
+            else if (rName == UNO_NAME_DOCUMENT_URL)
+                bOk &= rValue >>= msMailMergeDocumentURL;
         }
 
         CPPUNIT_ASSERT(bOk);
@@ -214,7 +219,7 @@ public:
         else
         {
             CPPUNIT_ASSERT(res == true);
-            if( !bMMFilenameFromColumn )
+            if( !bMMFilenameFromColumn && !bDontLoadResult )
                 loadMailMergeDocument( 0 );
         }
     }
@@ -252,7 +257,17 @@ public:
     */
     void loadMailMergeDocument( int number )
     {
-        OUString name = msMailMergeOutputPrefix + OUString::number( number ) + ".odt";
+        OUString name;
+        if (!msMailMergeOutputPrefix.isEmpty())
+            name = msMailMergeOutputPrefix;
+        else
+        {
+            INetURLObject aURLObj;
+            aURLObj.SetSmartProtocol( INetProtocol::File );
+            aURLObj.SetSmartURL( msMailMergeDocumentURL );
+            name = aURLObj.GetBase();
+        }
+        name += OUString::number( number ) + ".odt";
         loadMailMergeDocument( name );
     }
 
@@ -262,6 +277,7 @@ protected:
 
     uno::Reference< css::task::XJob > mxJob;
     std::vector< beans::NamedValue > mMMargs;
+    OUString msMailMergeDocumentURL;
     OUString msMailMergeOutputURL;
     OUString msMailMergeOutputPrefix;
     sal_Int16 mnCurOutputType;
@@ -601,6 +617,28 @@ DECLARE_FILE_MAILMERGE_TEST_COLUMN(testDirMailMerge, "simple-mail-merge.odt", "1
         CPPUNIT_ASSERT_EQUAL( OUString( "lastname" + OUString::number( doc )), getRun( getParagraph( 2 ), 1 )->getString());
         CPPUNIT_ASSERT_EQUAL( OUString( "Another fixed text." ), getRun( getParagraph( 3 ), 1 )->getString());
     }
+}
+
+DECLARE_FILE_MAILMERGE_TEST(testTdf102010, "empty.odt", "10-testing-addresses.ods", "testing-addresses")
+{
+    // Create "correct" automatic filename for non-user-supplied-prefix
+    for (auto aNamedValueIter = mMMargs.begin(); aNamedValueIter != mMMargs.end();)
+    {
+        if ( aNamedValueIter->Name == UNO_NAME_FILE_NAME_PREFIX )
+            aNamedValueIter = mMMargs.erase( aNamedValueIter );
+        else
+        {
+            std::cout << aNamedValueIter->Name << ": " << aNamedValueIter->Value << std::endl;
+            ++aNamedValueIter;
+        }
+    }
+    mMMargs.push_back( beans::NamedValue( OUString( UNO_NAME_SAVE_AS_SINGLE_FILE ), uno::Any( true ) ) );
+
+    // Generate correct mail merge result filename
+    executeMailMerge();
+    // Don't overwrite previous result
+    executeMailMerge( true );
+    loadMailMergeDocument( 1 );
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();