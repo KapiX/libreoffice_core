rohanKanojia/core

clarify logic plus lexis to be unaffiliated & independent from platform
Change-Id: Iaf202b3120636f6465206c696265726174696f6e

svx/source/svdraw/svdtrans.cxx
@@ -32,13 +32,27 @@ void MoveXPoly(XPolygon& rPoly, const Size& S)
     rPoly.Move(S.Width(),S.Height());
 }
 
+void ResizePoint(Point& rPnt, const Point& rRef, Fraction xFact, Fraction yFact)
+{
+    if ( !xFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction xFact looks too precise so use 1/1 as “don't scale”" );
+        xFact = Fraction(1,1);
+    }
+    if ( !yFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction yFact looks too precise so use 1/1 as “don't scale”" );
+        yFact = Fraction(1,1);
+    }
+    rPnt.X() = rRef.X() + svx::Round( (rPnt.X() - rRef.X()) * double(xFact) );
+    rPnt.Y() = rRef.Y() + svx::Round( (rPnt.Y() - rRef.Y()) * double(yFact) );
+}
+
 void ResizeRect(Rectangle& rRect, const Point& rRef, const Fraction& rxFact, const Fraction& ryFact)
 {
     Fraction xFact(rxFact);
     Fraction yFact(ryFact);
 
-    if (!xFact.IsValid()) {
-        SAL_WARN( "svx.svdraw", "invalid fraction xFract, using Fraction(1,1)" );
+    if ( !xFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction xFact looks too precise" );
         xFact = Fraction(1,1);
         long nWdt = rRect.Right() - rRect.Left();
         if (nWdt == 0) rRect.Right()++;
@@ -46,8 +60,8 @@ void ResizeRect(Rectangle& rRect, const Point& rRef, const Fraction& rxFact, con
     rRect.Left()  = rRef.X() + svx::Round( (rRect.Left()  - rRef.X()) * double(xFact) );
     rRect.Right() = rRef.X() + svx::Round( (rRect.Right() - rRef.X()) * double(xFact) );
 
-    if (!yFact.IsValid()) {
-        SAL_WARN( "svx.svdraw", "invalid fraction yFract, using Fraction(1,1)" );
+    if ( !yFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction yFact looks too precise" );
         yFact = Fraction(1,1);
         long nHgt = rRect.Bottom() - rRect.Top();
         if (nHgt == 0) rRect.Bottom()++;

sw/source/core/txtnode/fntcache.cxx
@@ -464,21 +464,18 @@ void SwFntObj::CreateScrFont( const SwViewShell& rSh, const OutputDevice& rOut )
 
     // check zoom factor, e.g. because of PrtOle2 during export
     {
-        // In case the zoom factor of the output device differs from the
-        // one in the ViewOptions, this Font must not be cached,
-        // hence set zoom factor to an invalid value
-        long nTmp;
-        if( pOut->GetMapMode().GetScaleX().IsValid() &&
-            pOut->GetMapMode().GetScaleY().IsValid() &&
+        sal_Int64 tmp;
+        if( pOut->GetMapMode().GetScaleX().IsOkay() &&
+            pOut->GetMapMode().GetScaleY().IsOkay() &&
             pOut->GetMapMode().GetScaleX() == pOut->GetMapMode().GetScaleY() )
         {
-            nTmp = ( 100 * pOut->GetMapMode().GetScaleX().GetNumerator() ) /
-                     pOut->GetMapMode().GetScaleX().GetDenominator();
+            tmp = ( 100 * pOut->GetMapMode().GetScaleX().GetNumerator() ) /
+                    pOut->GetMapMode().GetScaleX().GetDenominator();
         }
         else
-            nTmp = 0;
-        if( nTmp != nZoom )
-            nZoom = USHRT_MAX - 1;
+            tmp = 0;
+        if( tmp != nZoom ) // if zoom factor of the output device differs from the one in the ViewOptions
+            nZoom = USHRT_MAX - 1; // then don't cache this font, hence set zoom factor to special value
     }
 
     nScrAscent = (sal_uInt16)pOut->GetFontMetric().GetAscent();

sw/source/core/view/pagepreviewlayout.cxx
@@ -221,12 +221,12 @@ bool SwPagePreviewLayout::Init( const sal_uInt16 _nCols,
     {
         // adjust scaling for Drawing layer.
         aYScale *= Fraction( 1000, 1 );
-        long nNewNuminator = aYScale.operator long();
+        sal_Int64 nNewNuminator = aYScale.GetNumerator() / aYScale.GetDenominator();
         if( nNewNuminator < 1 )
-            nNewNuminator = 1;
+                nNewNuminator = 1;
         aYScale = Fraction( nNewNuminator, 1000 );
         // propagate scaling as zoom percentage to view options for font cache
-        _ApplyNewZoomAtViewShell( static_cast<sal_uInt8>(nNewNuminator/10) );
+        _ApplyNewZoomAtViewShell( static_cast< sal_uInt16 >( nNewNuminator / 10 ) );
 
         aMapMode.SetScaleY( aYScale );
         aMapMode.SetScaleX( aYScale );
@@ -245,7 +245,7 @@ bool SwPagePreviewLayout::Init( const sal_uInt16 _nCols,
 }
 
 /** apply new zoom at given view shell */
-void SwPagePreviewLayout::_ApplyNewZoomAtViewShell( sal_uInt8 _aNewZoom )
+void SwPagePreviewLayout::_ApplyNewZoomAtViewShell( sal_uInt16 _aNewZoom )
 {
     SwViewOption aNewViewOptions = *(mrParentViewShell.GetViewOptions());
     if ( aNewViewOptions.GetZoom() != _aNewZoom )

KapiX/libreoffice_core

svx/source/svdraw/svdtrans.cxx
@@ -37,8 +37,8 @@ void ResizeRect(Rectangle& rRect, const Point& rRef, const Fraction& rxFact, con
     Fraction aXFact(rxFact);
     Fraction aYFact(ryFact);
 
-    if (!aXFact.IsValid()) {
-        SAL_WARN( "svx.svdraw", "invalid fraction xFract, using Fraction(1,1)" );
+    if ( !aXFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction xFact looks too precise" );
         aXFact = Fraction(1,1);
         long nWdt = rRect.Right() - rRect.Left();
         if (nWdt == 0) rRect.Right()++;
@@ -46,8 +46,8 @@ void ResizeRect(Rectangle& rRect, const Point& rRef, const Fraction& rxFact, con
     rRect.Left()  = rRef.X() + svx::Round( (rRect.Left()  - rRef.X()) * double(aXFact) );
     rRect.Right() = rRef.X() + svx::Round( (rRect.Right() - rRef.X()) * double(aXFact) );
 
-    if (!aYFact.IsValid()) {
-        SAL_WARN( "svx.svdraw", "invalid fraction yFract, using Fraction(1,1)" );
+    if ( !aYFact.IsOkay() ) {
+        SAL_WARN( "svx.svdraw", "fraction yFact looks too precise" );
         aYFact = Fraction(1,1);
         long nHgt = rRect.Bottom() - rRect.Top();
         if (nHgt == 0) rRect.Bottom()++;

sw/source/core/txtnode/fntcache.cxx
@@ -465,16 +465,13 @@ void SwFntObj::CreateScrFont( const SwViewShell& rSh, const OutputDevice& rOut )
 
     // check zoom factor, e.g. because of PrtOle2 during export
     {
-        // In case the zoom factor of the output device differs from the
-        // one in the ViewOptions, this Font must not be cached,
-        // hence set zoom factor to an invalid value
-        long nTmp;
-        if( pOut->GetMapMode().GetScaleX().IsValid() &&
-            pOut->GetMapMode().GetScaleY().IsValid() &&
+        sal_Int64 nTmp;
+        if( pOut->GetMapMode().GetScaleX().IsOkay() &&
+            pOut->GetMapMode().GetScaleY().IsOkay() &&
             pOut->GetMapMode().GetScaleX() == pOut->GetMapMode().GetScaleY() )
         {
             nTmp = ( 100 * pOut->GetMapMode().GetScaleX().GetNumerator() ) /
-                     pOut->GetMapMode().GetScaleX().GetDenominator();
+                    pOut->GetMapMode().GetScaleX().GetDenominator();
         }
         else
             nTmp = 0;

sw/source/core/view/pagepreviewlayout.cxx
@@ -217,12 +217,12 @@ bool SwPagePreviewLayout::Init( const sal_uInt16 _nCols,
     {
         // adjust scaling for Drawing layer.
         aYScale *= Fraction( 1000, 1 );
-        long nNewNuminator = aYScale.operator long();
+        sal_Int64 nNewNuminator = aYScale.GetNumerator() / aYScale.GetDenominator();
         if( nNewNuminator < 1 )
-            nNewNuminator = 1;
+                nNewNuminator = 1;
         aYScale = Fraction( nNewNuminator, 1000 );
         // propagate scaling as zoom percentage to view options for font cache
-        ApplyNewZoomAtViewShell( static_cast<sal_uInt8>(nNewNuminator/10) );
+        ApplyNewZoomAtViewShell( static_cast< sal_uInt16 >( nNewNuminator / 10 ) );
 
         aMapMode.SetScaleY( aYScale );
         aMapMode.SetScaleX( aYScale );
@@ -241,7 +241,7 @@ bool SwPagePreviewLayout::Init( const sal_uInt16 _nCols,
 }
 
 /** apply new zoom at given view shell */
-void SwPagePreviewLayout::ApplyNewZoomAtViewShell( sal_uInt8 _aNewZoom )
+void SwPagePreviewLayout::ApplyNewZoomAtViewShell( sal_uInt16 _aNewZoom )
 {
     SwViewOption aNewViewOptions = *(mrParentViewShell.GetViewOptions());
     if ( aNewViewOptions.GetZoom() != _aNewZoom )