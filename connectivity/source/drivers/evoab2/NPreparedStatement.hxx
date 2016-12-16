three-comrades/core

tdf#88206 adapt to variadic templates from implbase.hxx & compbase.hxx
Change-Id: Ia09c94fdb35ddebab450a868c72a6909240d935f

connectivity/source/drivers/evoab2/NPreparedStatement.hxx
@@ -29,14 +29,14 @@
 #include <com/sun/star/sdbc/XResultSetMetaDataSupplier.hpp>
 #include <com/sun/star/sdbc/XPreparedBatchExecution.hpp>
 #include <com/sun/star/io/XInputStream.hpp>
-#include <cppuhelper/compbase5.hxx>
+#include <cppuhelper/compbase.hxx>
 
 namespace connectivity
 {
     namespace evoab
     {
 
-        typedef ::cppu::ImplHelper5< css::sdbc::XPreparedStatement,
+        typedef ::cppu::ImplHelper < css::sdbc::XPreparedStatement,
                                      css::sdbc::XParameters,
                                      css::sdbc::XResultSetMetaDataSupplier,
                                      css::sdbc::XMultipleResults,

KapiX/libreoffice_core

connectivity/source/drivers/evoab2/NPreparedStatement.hxx
@@ -29,14 +29,14 @@
 #include <com/sun/star/sdbc/XResultSetMetaDataSupplier.hpp>
 #include <com/sun/star/sdbc/XPreparedBatchExecution.hpp>
 #include <com/sun/star/io/XInputStream.hpp>
-#include <cppuhelper/implbase5.hxx>
+#include <cppuhelper/implbase.hxx>
 
 namespace connectivity
 {
     namespace evoab
     {
 
-        typedef ::cppu::ImplHelper5< css::sdbc::XPreparedStatement,
+        typedef ::cppu::ImplHelper < css::sdbc::XPreparedStatement,
                                      css::sdbc::XParameters,
                                      css::sdbc::XResultSetMetaDataSupplier,
                                      css::sdbc::XMultipleResults,